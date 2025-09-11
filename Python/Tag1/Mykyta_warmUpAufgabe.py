a43a5272b1dbdd7d9bf9401f3b67cfc924e93103
nimkaaa <vasiletsnikita39@gmail.com>
2025-09-11 13:12:23 +0200

    added message section

---
+++ Python/Tag1/Mykyta_warmUpAufgabe.py
@@ -0,0 +1,16 @@
+print("Hello\nWhat's your name?")
+
+name = input()
+
+print("Hello " + name + "! It's nice to meet you.")
+print("How old are you?")
+
+age = input()
+
+print("Wow, " + age + " years old! You look great for your age. I'm a little older than you, I'm 25.")
+print("What is your favorite programming language?")
+
+favorite_language = input()
+
+print("That's great! " + favorite_language + " is a popular language.")
+print("It was nice chatting with you, " + name + "! Have a great day!")
---
+++ Python/Tag2/Mykyta_wuerfel.py
@@ -0,0 +1,5 @@
+import random
+
+cube = random.randint(1, 6)
+
+print(cube)
