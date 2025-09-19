09b54d4e6cd359f9a9734c81242abde8f72f5868
nimkaaa <vasiletsnikita39@gmail.com>
2025-09-19 13:08:33 +0200

    wrote if-conditionals about age and movies 18+

--- Python/Tag3/Mykyta_ifConditionals.py
+++ Python/Tag3/Mykyta_ifConditionals.py
@@ -12,7 +12,7 @@ def isAdult(month, year):
    years = CURRENT_YEAR - year
    if years > 18:
       return True
-   elif years == 18:
+   elif years == 18 and legal_age:
       return month <= CURRENT_MONTH
    else:
       return False
@@ -20,21 +20,21 @@ def isAdult(month, year):
 def isHorror(wish):
    return wish == "yes"
 
+def calculateMonths(month, year):
+   user_months = (CURRENT_YEAR - year - 1) * 12 + month #узнаем кол-во месяцев
+   return 216 - user_months
+
 #=====================START=========================
 
-user_country = input("Is it considered 18 years in your country to age?\n(yes/no)\n")
+user_country = input("Is it considered 18 years in your country to be legal age?\n(yes/no)\n")
 
-if isLegit(user_country):
+legal_age = isLegit(user_country)
+
+user_month = int(input("In which month were you born? (give as digits)\n"))
+user_year = int(input("In which year were you born?\n"))
+
+if isAdult(user_month, user_year):
    print("All right, let's proceed. ...")
-   user_month = int(input("In which month were you born? (give as digits)\n"))
-   user_year = int(input("In which year were you born?\n"))
-   if isAdult(user_month, user_year):
-      print("All right, let's proceed. ...")
-      user_wish = input("Do you wanna watch horror movie (18+)?\n(yes/no)\n")
-   else:
-      print("Thanks for answering! Have a nice childhood! Bye!")
+   user_wish = input("Do you wanna watch horror movie (18+) or something else?\n(horror/another movie)\n")
 else:
-   print("Thanks for answering! Have a nice childhood! Bye!")
-
-# arbeite noch dran...
-
+   print(f"Sorry, yor are too young, but you can come back in {calculateMonths(user_month, user_year)} months")
